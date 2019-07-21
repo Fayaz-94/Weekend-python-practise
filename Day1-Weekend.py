1df313e7852569e33332e3c07ebc214baca9b5a7
Fayaz-94 <52184008+Fayaz-94@users.noreply.github.com>
2019-07-21 20:24:07 +0530

    Add files via upload

---
+++ Day1-Weekend.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#how to check the version of python we are using currently??
+
+import sys
+
+
+# In[2]:
+
+
+print("Hello world!")
+
+
+# In[3]:
+
+
+#introduction to variables ....
+
+x=10
+y =5
+
+x+y
+
+
+# In[4]:
+
+
+message = "Hello python world!"
+
+print(message)
+
+
+# In[ ]:
+
+
+#Introduction to Data types in python!!
+
+#1. Strings..
+
+
+# In[5]:
+
+
+# introduction to strings
+
+#--A String is a series of charcaters.
+
+# A string is an immutable data type.
+
+#how to define a String?
+
+#Anything inside a quote is considered as a string...
+
+name ="tony stark"
+
+print(name)
+
+
+# In[8]:
+
+
+first_name = "Shaik"
+last_name = "Faiyazuddin"
+
+print(first_name)
+
+#1.can we declare or assign a space while giving the variable name...?
+
+
+# In[12]:
+
+
+name ="tony stark"
+
+print(name.upper())
+
+#..how do you change the input to tiltlecase , uppercase and lowercase
+
+
+# In[14]:
+
+
+#using variables in the strings 3.6 above version...
+
+first_name ="Tony"
+last_name = "stark"
+# F- Strings...we can be able to assign references to other variables as well
+full_name = f"{first_name} {last_name}"
+
+print(full_name.title())
+
+
+# In[15]:
+
+
+#adding whitespace to strings with tabs and newlines.....
+
+#any non printing charcater...!!
+
+print("python")
+
+
+# In[16]:
+
+
+print("python ")
+
+
+# In[17]:
+
+
+print('\tpython')
+
+#method of declaring a string...
+
+#double quote, single quote, triple quote
+
+
+declare = "somestring"
+
+declare1 = 'somestring1'
+
+declare2 = """somestring2"""
+
+example = "john's dad name is unique"
+
+--interview question: how do you include afstophe in between the strings..!1
+
+
+# In[19]:
+
+
+#how to print the output on the newline...!!
+
+print("Languages:\npython\nC\nJava\nc++\nswift")
+
+
+# In[21]:
+
+
+#stripping the whitespaces!!
+
+fav_lang = "python "
+
+print(fav_lang.rstrip())
+
+
+# In[ ]:
+
+
+
+
